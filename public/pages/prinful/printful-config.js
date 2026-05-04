44371525ad860f10bd565448f5f721a2ecfdf106
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-05-04 00:40:58 -0400

    Create printful-config.js

---
+++ public/pages/prinful/printful-config.js
@@ -0,0 +1,8 @@
+// /pages/printful/printful-config.js
+
+// 🔐 INSERT YOUR PRINTFUL PRIVATE TOKEN HERE
+// Example: export const PRINTFUL_TOKEN = "pf_xxxxxxxxxxxxxxxxx";
+export const PRINTFUL_TOKEN = "YOUR_PRINTFUL_PRIVATE_TOKEN_HERE";
+
+// Base API URL
+export const PRINTFUL_BASE_URL = "https://api.printful.com";
