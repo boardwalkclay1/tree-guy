e6f2e7833f09144b5ffcc17d95ba007ab858dcc5
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-07-16 20:48:32 -0400

    Add weather API handling to worker.js

--- src/worker.js
+++ src/worker.js
@@ -2,6 +2,7 @@ import * as MapLogic from "./work-map.js";
 import * as CalendarLogic from "./work-calendar.js";
 import * as ContractLogic from "./work-contracts.js";
 import * as RadioLogic from "./work-radio.js";
+import * as WeatherLogic from "./work-weather.js";
 
 export default {
   async fetch(request, env) {
@@ -26,6 +27,11 @@ export default {
     // RADIO (signaling + presence)
     if (path.startsWith("/api/radio")) {
       return RadioLogic.handle(request, env);
+    }
+
+    // WEATHER (profile + cached forecast)
+    if (path.startsWith("/api/weather")) {
+      return WeatherLogic.handle(request, env);
     }
 
     return new Response("Not found", { status: 404 });
